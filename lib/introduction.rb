4d8185becbe12e160bac1a06c0206ef9819cc152
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 06:45:36 +0000

    automatically backed up by learn

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name)
+  name = "Dan"
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name)
+  name = "Dan"
+  language = "Node.js"
+  puts "Hi, my name is #{name} and I am learning to program #{Node.js}."
+end
